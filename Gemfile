61d4c9f02b7f88d63ed5fc490cc14c9f9fa4c917
Ethan Rosenberg <ethan@ethanrosenberg.com>
2020-04-11 00:42:09 -0700

    Added Searches controller and Search model keyword:string. Also added ActiveAdmin UI

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ gem 'dotenv-rails'
 gem 'tree_support'
 gem 'ruby-graphviz'
 gem 'rack-cors'
-
+gem 'activeadmin'
+gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -6,4 +6,12 @@ class Keyword < ApplicationRecord
     #words = self.base.split(/[^[[:word:]]]+/)
   #end
 
+  def created_at
+    self[:created_at].in_time_zone('Pacific Time (US & Canada)').strftime("%B %d, %Y %l:%M %p")
+  end
+
+  def updated_at
+    self[:updated_at].in_time_zone('Pacific Time (US & Canada)').strftime("%B %d, %Y %l:%M %p")
+  end
+
 end
